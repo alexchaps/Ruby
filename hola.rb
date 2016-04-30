ce02db4d7deef8d2998fbb6abd84dfd9bd9f115b
alexchaps <alex.chapa.juarez@gmail.com>
2016-04-30 16:33:42 -0500

    Manual primera parte

--- hola.rb
+++ hola.rb
@@ -7,3 +7,52 @@ puts "Iniciaremos con los tipos basicos"
 #Sumas
 
 puts 34+6
+
+puts 40-9
+
+puts -9+8
+
+puts 34+54+22
+
+puts 5.5+3.7
+
+puts 3.45+6.55
+
+#Restas
+
+puts 34-20
+
+puts -10-9
+
+puts -9-7-10
+
+puts 5.0-1.5
+
+#Multiplicación
+
+puts 9*9
+
+puts 10*2*3
+
+puts -8*-7
+
+puts -3*3
+
+puts 3.2*2
+
+puts 3.2*2.1
+
+#División
+
+puts 5/2
+
+puts -4/3
+
+puts 5.0/2
+
+puts 5/2.0
+
+puts -4.0/3
+
+
+
